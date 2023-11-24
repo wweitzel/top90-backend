f44b128f569af340c806304370fb90f5d43ec25d
Wes Weitzel <wesweitzel@gmail.com>
2023-11-24 09:56:13 -0800

    Refactor fixture determination

--- internal/db/dao_test.go
+++ internal/db/dao_test.go
@@ -1,22 +1,14 @@
 package db
 
 import (
-	"database/sql"
-	"fmt"
-	"log"
 	"testing"
 	"time"
 
-	"github.com/golang-migrate/migrate/v4"
-	"github.com/golang-migrate/migrate/v4/database/postgres"
 	_ "github.com/golang-migrate/migrate/v4/source/file"
 	"github.com/google/uuid"
 	top90 "github.com/wweitzel/top90/internal"
 	"github.com/wweitzel/top90/internal/apifootball"
 	"gotest.tools/v3/assert"
-
-	"github.com/ory/dockertest/v3"
-	"github.com/ory/dockertest/v3/docker"
 )
 
 func TestGetGoals(t *testing.T) {
@@ -222,68 +214,6 @@ func TestGetLeagues(t *testing.T) {
 	})
 	assert.NilError(t, err)
 
-	leagues, err := dao.GetLeagues()
+	leagues, _ := dao.GetLeagues()
 	assert.Equal(t, len(leagues), 2)
-}
-
-func setup() (dao Top90DAO, pool *dockertest.Pool, res *dockertest.Resource) {
-	pool, err := dockertest.NewPool("")
-	if err != nil {
-		log.Fatalf("Could not construct pool: %s", err)
-	}
-
-	err = pool.Client.Ping()
-	if err != nil {
-		log.Fatalf("Could not connect to Docker: %s", err)
-	}
-
-	resource, err := pool.RunWithOptions(&dockertest.RunOptions{
-		Repository: "postgres",
-		Tag:        "13",
-		Env: []string{
-			"POSTGRES_PASSWORD=secret",
-			"POSTGRES_USER=user_name",
-			"POSTGRES_DB=dbname",
-			"listen_addresses = '*'",
-		},
-	}, func(config *docker.HostConfig) {
-		config.AutoRemove = true
-		config.RestartPolicy = docker.RestartPolicy{Name: "no"}
-	})
-
-	if err != nil {
-		log.Fatalf("Could not start resource: %s", err)
-	}
-
-	hostAndPort := resource.GetHostPort("5432/tcp")
-	databaseUrl := fmt.Sprintf("postgres://user_name:secret@%s/dbname?sslmode=disable", hostAndPort)
-
-	resource.Expire(120)
-
-	var db *sql.DB
-
-	// exponential backoff-retry, because the application in the container might not be ready to accept connections yet
-	pool.MaxWait = 120 * time.Second
-	if err = pool.Retry(func() error {
-		db, err = sql.Open("postgres", databaseUrl)
-		if err != nil {
-			return err
-		}
-		return db.Ping()
-	}); err != nil {
-		log.Fatalf("Could not connect to docker: %s", err)
-	}
-
-	driver, err := postgres.WithInstance(db, &postgres.Config{})
-
-	var mig *migrate.Migrate
-	mig, err = migrate.NewWithDatabaseInstance(
-		"file://migrations",
-		"postgres", driver)
-
-	if err := mig.Up(); err != nil {
-		log.Fatal(err)
-	}
-
-	return NewPostgresDAO(db), pool, resource
 }
--- internal/ingest/fixture_determination.go
+++ internal/ingest/fixture_determination.go
@@ -7,102 +7,32 @@ import (
 	"github.com/wweitzel/top90/internal/apifootball"
 )
 
-func GetTeamName(redditPostTitle string) (string, error) {
-	// TODO: There has to be a better way than this
-	index0 := strings.IndexByte(redditPostTitle, '0')
-	index1 := strings.IndexByte(redditPostTitle, '1')
-	index2 := strings.IndexByte(redditPostTitle, '2')
-	index3 := strings.IndexByte(redditPostTitle, '3')
-	index4 := strings.IndexByte(redditPostTitle, '4')
-	index5 := strings.IndexByte(redditPostTitle, '5')
-	index6 := strings.IndexByte(redditPostTitle, '6')
-	index7 := strings.IndexByte(redditPostTitle, '7')
-	index8 := strings.IndexByte(redditPostTitle, '8')
-	index9 := strings.IndexByte(redditPostTitle, '9')
-	indexBracket := strings.IndexByte(redditPostTitle, '[')
-
-	lowestIndex := 9999999
-	if index0 != -1 && index0 < lowestIndex {
-		lowestIndex = index0
-	}
-	if index1 != -1 && index1 < lowestIndex {
-		lowestIndex = index1
-	}
-	if index2 != -1 && index2 < lowestIndex {
-		lowestIndex = index2
-	}
-	if index3 != -1 && index3 < lowestIndex {
-		lowestIndex = index3
-	}
-	if index4 != -1 && index4 < lowestIndex {
-		lowestIndex = index4
-	}
-	if index5 != -1 && index5 < lowestIndex {
-		lowestIndex = index5
-	}
-	if index6 != -1 && index6 < lowestIndex {
-		lowestIndex = index6
-	}
-	if index7 != -1 && index7 < lowestIndex {
-		lowestIndex = index7
-	}
-	if index8 != -1 && index8 < lowestIndex {
-		lowestIndex = index8
-	}
-	if index9 != -1 && index9 < lowestIndex {
-		lowestIndex = index9
-	}
-	if indexBracket != -1 && indexBracket < lowestIndex {
-		lowestIndex = indexBracket
-	}
-
-	if lowestIndex == 9999999 {
-		return "", errors.New("could not determine team name")
-	}
-
-	teamName := strings.TrimSpace(redditPostTitle[:lowestIndex])
-	return teamName, nil
-}
-
-func GetTeamForTeamName(teamName string, teams []apifootball.Team) (apifootball.Team, error) {
-	for _, t := range teams {
-		if t.Name == teamName {
-			return t, nil
-		}
-
-		for _, alias := range t.Aliases {
-			if alias == teamName {
-				return t, nil
+func findTeams(redditPostTile string, teams []apifootball.Team) []apifootball.Team {
+	var teamsFound []apifootball.Team
+	for _, team := range teams {
+		if strings.Contains(redditPostTile, team.Name) {
+			teamsFound = append(teamsFound, team)
+		} else {
+			for _, alias := range team.Aliases {
+				if strings.Contains(redditPostTile, alias) {
+					teamsFound = append(teamsFound, team)
+				}
 			}
 		}
 	}
-
-	return apifootball.Team{}, errors.New("could not determine team")
+	return teamsFound
 }
 
-func GetFixtureForTeamName(teamName string, aliases []string, fixtures []apifootball.Fixture) (apifootball.Fixture, error) {
-	var fixture apifootball.Fixture
-	var foundFixture bool
+func FindFixture(redditPostTitle string, allTeams []apifootball.Team, fixtures []apifootball.Fixture) (apifootball.Fixture, error) {
+	teams := findTeams(redditPostTitle, allTeams)
 
 	for _, f := range fixtures {
-		if f.Teams.Home.Name == teamName || f.Teams.Away.Name == teamName {
-			fixture = f
-			foundFixture = true
-			break
-		}
-
-		for _, alias := range aliases {
-			if f.Teams.Home.Name == alias || f.Teams.Away.Name == alias {
-				fixture = f
-				foundFixture = true
-				break
+		for _, t := range teams {
+			if f.Teams.Home.Id == t.Id || f.Teams.Away.Id == t.Id {
+				return f, nil
 			}
 		}
 	}
 
-	if !foundFixture {
-		return apifootball.Fixture{}, errors.New("could not determine fixture")
-	}
-
-	return fixture, nil
+	return apifootball.Fixture{}, errors.New("could not determine fixture")
 }
