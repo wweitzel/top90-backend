483212dbd7e6c906398a806ae042965efcd1f488
Wes Weitzel <wesweitzel@gmail.com>
2023-12-03 17:45:23 -0800

    Use cloudfront urls (#77)

--- internal/api/handlers/message.go
+++ internal/api/handlers/message.go
@@ -1,9 +1,7 @@
 package handlers
 
 import (
-	"log"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/wweitzel/top90/internal/clients/s3"
@@ -27,15 +25,8 @@ func (h *MessageHandler) GetPreview(w http.ResponseWriter, r *http.Request) {
 	id := mux.Vars(r)["id"]
 	goal, _ := h.dao.GetGoal(id)
 
-	// TODO: This is nearly duplicated with the code in getGoals and getGoal
-	videoUrl, err := h.s3Client.NewSignedGetURL(goal.S3ObjectKey, h.s3Bucket, time.Hour*24*7)
-	if err != nil {
-		log.Println(err)
-	}
-	thumbnailUrl, err := h.s3Client.NewSignedGetURL(goal.ThumbnailS3Key, h.s3Bucket, time.Hour*24*7)
-	if err != nil {
-		log.Println(err)
-	}
+	thumbnailUrl := "https://s3-redditsoccergoals.top90.io/" + goal.ThumbnailS3Key
+	videoUrl := "https://s3-redditsoccergoals.top90.io/" + goal.S3ObjectKey
 
 	goal.PresignedUrl = videoUrl
 	goal.ThumbnailPresignedUrl = thumbnailUrl
@@ -49,8 +40,8 @@ func (h *MessageHandler) GetPreview(w http.ResponseWriter, r *http.Request) {
 			// rather than returning the presigned url since we don't really want these to expire.
 			// Max expiry date for presigned url is apparently one week.
 			`	<meta property="og:title" content="` + goal.RedditPostTitle + `"` + `>` +
-			`	<meta property="og:image" content="` + goal.ThumbnailPresignedUrl + `"` + `>` +
-			`	<meta property="og:video" content="` + goal.PresignedUrl + `"` + `>` +
+			`	<meta property="og:image" content="` + thumbnailUrl + `"` + `>` +
+			`	<meta property="og:video" content="` + videoUrl + `"` + `>` +
 			` <meta http-equiv="refresh" content="0; url='https://top90.io/goals/` + goal.Id + `'" />` +
 			` <title>top90.io</title>` +
 			`</head>` +
