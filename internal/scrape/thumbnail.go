1cc97a50597ab8dd120f88baa4d92443031f9cf1
Wes Weitzel <wesweitzel@gmail.com>
2023-12-04 09:01:36 -0800

    Switch to jpg (#80)

--- internal/scrape/thumbnail.go
+++ internal/scrape/thumbnail.go
@@ -13,10 +13,10 @@ import (
 func extractThumbnail(video *os.File) (filename string) {
 	randomId := uuid.NewString()
 	randomId = strings.Replace(randomId, "-", "", -1)
-	thumbnailFilename := fmt.Sprintf("tmp/%s.avif", randomId)
+	thumbnailFilename := fmt.Sprintf("tmp/%s.jpg", randomId)
 
 	ffmpegPath := os.Getenv("TOP90_FFMPEG_PATH")
-	cmd := exec.Command(ffmpegPath, "-i", video.Name(), "-q:v", "2", "-vframes", "1", thumbnailFilename)
+	cmd := exec.Command(ffmpegPath, "-i", video.Name(), "-q:v", "8", "-vframes", "1", thumbnailFilename)
 	cmd.Stderr = os.Stdout
 	cmd.Stdout = os.Stdout
 
