cb21a0c2ba7eeba66e5fef065e9f51808ad5ba8c
Wes Weitzel <wesweitzel@gmail.com>
2023-12-09 15:32:12 -0800

    Create indexes (#106)

---
+++ internal/db/migrations/000009_create_indexes.down.sql
@@ -0,0 +1,7 @@
+DROP INDEX IF EXISTS goals_reddit_post_created_at;
+DROP INDEX IF EXISTS goals_fixture_id_reddit_post_created_at;
+
+DROP INDEX IF EXISTS fixtures_date;
+DROP INDEX IF EXISTS fixtures_league_id;
+DROP INDEX IF EXISTS fixtures_season;
+DROP INDEX IF EXISTS fixtures_league_id_season;
