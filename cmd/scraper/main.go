52b9808d829cd53d36cd0971004ecf2496b3bce0
Wes Weitzel <wesweitzel@gmail.com>
2025-04-18 19:40:26 -0700

    Add debug logging

--- cmd/scraper/main.go
+++ cmd/scraper/main.go
@@ -18,8 +18,11 @@ func main() {
 		Colorize: config.LogColor,
 	})
 
+	logger.Info("Starting initialization")
+
 	init := cmd.NewInit(logger)
 
+	logger.Info("Initializing S3 client...")
 	s3Client := init.S3Client(s3.Config{
 		AccessKey:       config.AwsAccessKey,
 		SecretAccessKey: config.AwsSecretAccessKey,
@@ -27,8 +30,14 @@ func main() {
 		Logger:          logger,
 	}, config.AwsBucketName)
 
+	logger.Info("Done")
+
+	logger.Info("Initializing Reddit client...")
 	redditClient := init.RedditClient(10 * time.Second)
 
+	logger.Info("Done")
+
+	logger.Info("Initializing database...")
 	db := init.DB(
 		config.DbUser,
 		config.DbPassword,
@@ -36,11 +45,19 @@ func main() {
 		config.DbHost,
 		config.DbPort)
 
+	logger.Info("Done")
+
+	logger.Info("Initializing dao...")
 	dao := init.Dao(db)
 
+	logger.Info("Done")
+
+	logger.Info("Initializing chromdp...")
 	chromeCtx, cancel := init.ChromeDP()
 	defer cancel()
+	logger.Info("Done")
 
+	logger.Info("Initializing apifootball client...")
 	var apifbClient *apifootball.Client
 	if config.ApiFootballPlayerLinkEnabled {
 		apifbClient = init.ApiFootballClient(
@@ -51,6 +68,9 @@ func main() {
 			config.ApiFootballCurrentSeason)
 	}
 
+	logger.Info("Done")
+
+	logger.Info("Initializing scraper...")
 	scraper := scrape.NewScraper(
 		chromeCtx,
 		dao,
@@ -59,6 +79,8 @@ func main() {
 		config.AwsBucketName,
 		apifbClient,
 		logger)
+
+	logger.Info("Done...")
 
 	err := scraper.ScrapeNewPosts()
 	if err != nil {
